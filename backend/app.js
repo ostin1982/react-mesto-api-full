f02d79be1dbee96b3401167ba62d06a533aca753
Denis Sokolov <ostin1982@yandex.ru>
2021-04-01 17:09:34 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,11 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const expressWinston = require('express-winston');
 const winston = require('winston');
-const { celebrate, errors, Joi } = require('celebrate');
+const { celebrate, CelebrateError, Joi } = require('celebrate');
 
 const router = require('./routes/router');
 const { login, createUser } = require('./controllers/users');
+const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
 
 const app = express();
@@ -21,6 +22,15 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
+const options = [
+  'http://ostin.student.nomoredomains.club',
+  'http://www.ostin.student.nomoredomains.club',
+  'https://www.ostin.student.nomoredomains.club',
+  'https://ostin.student.nomoredomains.club',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
 
 const requestLogger = expressWinston.logger({
   transports: [
@@ -36,18 +46,33 @@ const errorLogger = expressWinston.errorLogger({
   format: winston.format.json(),
 });
 
-// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   if (err.status) {
     return res.status(err.status).send({ message: err.message });
   }
-  return res.status(500).send({ message: err.message });
+  if (err instanceof CelebrateError) {
+    return res.status(400).send({ message: `Переданы неверные данные: ${err}` });
+  }
+  res.status(500).send({ message: err.message });
+  return next();
 };
 
 app.use(requestLogger);
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (options.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -74,7 +99,11 @@ createUser);
 app.use(auth);
 app.use('/', router);
 app.use(errorLogger);
-app.use(errors());
+
+app.use(() => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
 app.use(errorHandler);
 
 app.listen(PORT);
