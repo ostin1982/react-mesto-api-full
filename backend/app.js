dada11c262d4731915e0343c39633bbd26568fbb
Denis Sokolov <ostin1982@yandex.ru>
2021-04-01 17:12:55 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const helmet = require('helmet');
 const expressWinston = require('express-winston');
 const winston = require('winston');
 const { celebrate, CelebrateError, Joi } = require('celebrate');
@@ -20,7 +20,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
-  useUnifiedTopology: true,
 });
 
 const options = [
