e9c81581ae602445edeb8ce8d7d35e9641e3fcd6
Denis Sokolov <ostin1982@yandex.ru>
2021-04-01 17:18:08 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,25 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const expressWinston = require('express-winston');
 const winston = require('winston');
-const { celebrate, CelebrateError, Joi } = require('celebrate');
+const { celebrate, errors, Joi } = require('celebrate');
 
 const router = require('./routes/router');
 const { login, createUser } = require('./controllers/users');
-const NotFoundError = require('./errors/NotFoundError');
-const auth = require('./middlewares/auth');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 });
-
-const options = [
-  'http://ostin.student.nomoredomains.club',
-  'http://www.ostin.student.nomoredomains.club',
-  'https://www.ostin.student.nomoredomains.club',
-  'https://ostin.student.nomoredomains.club',
-  'http://localhost:3000',
-  'http://localhost:3001',
-];
 
 const requestLogger = expressWinston.logger({
   transports: [
@@ -45,33 +35,18 @@ const errorLogger = expressWinston.errorLogger({
   format: winston.format.json(),
 });
 
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   if (err.status) {
     return res.status(err.status).send({ message: err.message });
   }
-  if (err instanceof CelebrateError) {
-    return res.status(400).send({ message: `Переданы неверные данные: ${err}` });
-  }
-  res.status(500).send({ message: err.message });
-  return next();
+  return res.status(500).send({ message: err.message });
 };
 
-app.use(cors({
-  origin: options,
-}));
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (options.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -80,10 +55,10 @@ app.get('/crash-test', () => {
 });
 
 app.post('/signin', celebrate({
-  body: {
+  body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(2),
-  },
+    password: Joi.string().required().min(8),
+  }),
 }),
 login);
 
@@ -91,21 +66,13 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/),
-    about: Joi.string().min(2).max(30),
   }),
 }),
 createUser);
 
-app.use(auth);
 app.use('/', router);
 app.use(errorLogger);
-
-app.use(() => {
-  throw new NotFoundError('Запрашиваемый ресурс не найден');
-});
-
 app.use(errorHandler);
+app.use(errors());
 
 app.listen(PORT);
