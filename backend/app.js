3ea4bf37fa24d38671df233fff2b04df2981a888
Denis Sokolov <ostin1982@yandex.ru>
2021-04-01 17:11:50 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const auth = require('./middlewares/auth');
 
 const app = express();
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -57,8 +57,6 @@ const errorHandler = (err, req, res, next) => {
   return next();
 };
 
-app.use(requestLogger);
-app.use(helmet());
 app.use(cors({
   origin: options,
 }));
