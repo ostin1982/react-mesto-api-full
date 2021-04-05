29596c47f55e84d9b55419f20d2ccc275a6acbb1
Denis Sokolov <ostin1982@yandex.ru>
2021-04-05 14:36:51 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -60,9 +60,9 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
     res.send(200);
   }
