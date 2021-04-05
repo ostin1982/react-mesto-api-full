cc1136d94d55c341dab63ef630669d30b3e49c31
Denis Sokolov <ostin1982@yandex.ru>
2021-04-05 16:44:22 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,25 +1,26 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../config/config');
 const AuthenticationError = require('../errors/AuthenticationError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) {
-    throw new AuthenticationError('Необходима авторизация');
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthenticationError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
   } catch (err) {
-    throw new AuthenticationError('Необходима авторизация');
+    return next(new AuthenticationError('Необходима авторизация'));
   }
 
   req.user = payload;
-
   next();
 };
 
