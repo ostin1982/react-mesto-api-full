83c2c5526ad6f4366d3e2ee0e6b3e0c439eded7c
Denis Sokolov <ostin1982@yandex.ru>
2021-04-07 16:52:32 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
