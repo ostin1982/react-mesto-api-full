142cc22d09c45c9f8fb8f727a143c5142e6451a9
Denis Sokolov <ostin1982@yandex.ru>
2021-04-07 12:00:37 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -27,7 +27,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use((req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': [
-      'http://localhost:3001',
+      'http://localhost:3000',
       'https://ostin.student.nomoredomains.club',
       'http://ostin.student.nomoredomains.club',
     ],
