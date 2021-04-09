0329a17d8390d6f9dd0f41914942ba81c7a1bd87
Denis Sokolov <ostin1982@yandex.ru>
2021-04-09 21:21:46 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -25,10 +25,6 @@ const options = {
     'https://ostin.student.nomoredomains.club',
     'http://ostin.student.nomoredomains.club',
   ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
   credentials: true,
 };
 
@@ -40,7 +36,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use('*', cors(options));
-app.use(helmet());
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
