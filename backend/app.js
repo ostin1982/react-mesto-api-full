b871e61b0b8d2cf4c7b18a0887ff51d666ab4d7f
Denis Sokolov <ostin1982@yandex.ru>
2021-04-11 10:09:18 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -18,16 +18,24 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  }
+  next();
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
-  autoIndex: true,
+  useUnifiedTopology: true,
 });
 
-app.use(cors({ origin: true }));
-app.use(bodyParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(expressWinston.logger({
   transports: [
