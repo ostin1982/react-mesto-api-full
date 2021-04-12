1d7a2f4fbe2ccd54d07fbe0b2eb9b4e369135f47
Denis Sokolov <ostin1982@yandex.ru>
2021-04-12 17:40:07 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,38 +4,38 @@ const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getProfile, createProfile, updateProfile, updateAvatar, login, createUser,
 } = require('../controllers/users');
-const auth = require('../middlewares/auth');
 
-router.get('/', auth, getUsers);
-router.get('/:userId', auth, getProfile);
-router.get('/', celebrate({
+router.get('/users', getUsers);
+router.get('/users/:userId', getProfile);
+router.get('/users', celebrate({
   params: {
     userId: Joi.string().required().length(24).hex(),
   },
-}), createProfile);
+}),
+createProfile);
 
-router.patch('/me', auth, celebrate({
+router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30).required(),
   }),
 }), updateProfile);
 
-router.patch('/me/avatar', auth, celebrate({
+router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     // eslint-disable-next-line
     avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[\w\-\/\.a-z#?]{1,}/i).required(),
   }),
 }), updateAvatar);
 
-router.post('/signin', auth, celebrate({
+router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
 
-router.post('/signup', auth, celebrate({
+router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
