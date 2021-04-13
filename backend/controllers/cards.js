512ca487c352a535decc892041193d4c3594ee10
Denis Sokolov <ostin1982@yandex.ru>
2021-04-13 18:21:04 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -2,7 +2,6 @@ const Card = require('../models/card');
 const ValidationError = require('../errors/ValidationError');
 const ProfileError = require('../errors/ProfileError');
 const NotFoundError = require('../errors/NotFoundError');
-const RegistrationError = require('../errors/RegistrationError');
 
 const getCards = (req, res, next) => Card.find({})
   .then((cards) => res.status(200).send(cards))
@@ -28,29 +27,15 @@ const createCard = (req, res, next) => {
 };
 
 const deleteCard = (req, res, next) => {
-  const { cardId } = req.params;
-
-  Card.findById(cardId)
-    .orFail()
-    .then((card) => {
-      if (String(card.owner) !== req.user._id && req.user._id !== card.owner) {
-        return next(new ProfileError('У вас нет прав на данное дейтвие'));
-      }
-      return Card.findByIdAndRemove(cardId)
-        .orFail()
-        .then(() => res.send({ message: 'Карточка удалена' }));
-    })
+  Card.findByIdAndRemove(req.params)
+    .orFail(new NotFoundError('Нет карточки с такими данными'))
+    .then((card) => res.status(200).send(card))
     .catch((err) => {
-      if (err.name === 'MongoError' && err.code === 11000) {
-        next(new RegistrationError('Пользователь с такими данными уже зарегистирирован'));
+      if (err.name === 'CastError' || err.message === 'Not found') {
+        throw new ProfileError('У вас нет прав на данное дейтвие');
       }
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
-        next(new ValidationError('Ошибка в заполнении полей'));
-      }
-      if (err.name === 'DocumentNotFoundError') {
-        next(new NotFoundError('Карточки с данным id не существует'));
-      }
-    });
+    })
+    .catch(next);
 };
 
 const likeCard = (req, res, next) => Card.findByIdAndUpdate(
