3f5efc7c871aa2458b0e5b7a818c5ee99a7a91d8
Denis Sokolov <ostin1982@yandex.ru>
2021-04-13 15:01:12 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -43,22 +43,19 @@ app.use(expressWinston.logger({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  }),
-}),
-login);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/),
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
   }),
 }),
 createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}),
+login);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
