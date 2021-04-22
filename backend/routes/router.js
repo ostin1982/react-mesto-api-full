ccaad0e2f9a790ff966878c9a1cffd20f2bd537d
Denis Sokolov <ostin1982@yandex.ru>
2021-04-22 19:12:45 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,9 +4,8 @@ const cardsRouter = require('./cards');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
-router.use(auth);
+router.use('/users', auth, usersRouter);
+router.use('/cards', auth, cardsRouter);
 router.use('*', (req, res, next) => {
   next(new NotFoundError({ message: 'Запрашиваемый ресурс не найден' }));
 });
