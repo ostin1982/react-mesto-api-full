c6e7bd6c571d0c15616a18bacb3367c5fac06615
Denis Sokolov <ostin1982@yandex.ru>
2021-04-23 17:52:15 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,6 +7,7 @@ const User = require('../models/user');
 const RegistrationError = require('../errors/RegistrationError');
 const NotFoundError = require('../errors/NotFoundError');
 const ValidationError = require('../errors/ValidationError');
+const AuthenticationError = require('../errors/AuthenticationError');
 
 const getUsers = (req, res, next) => {
   User.findById({})
@@ -85,15 +86,22 @@ const updateAvatar = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
-  return User.findUserByCredentials(email, password)
+  User.findOne({ email }).select('+password')
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        JWT_SECRET,
-        { expiresIn: '7d' },
-      );
-
-      res.send({ token });
+      if (!user) {
+        throw new AuthenticationError('Необходима авторизация!!');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (matched) {
+            return user;
+          }
+          throw new AuthenticationError('Необходима авторизация!');
+        });
+    })
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      res.send({ jwt: token });
     })
     .catch(next);
 };
