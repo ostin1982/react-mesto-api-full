3c5a08d3d6404f7fd13cec4c8170f76a921da2cf
Denis Sokolov <ostin1982@yandex.ru>
2021-04-23 19:45:29 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
-
-const AuthenticationError = require('../errors/AuthenticationError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -14,7 +11,7 @@ const userSchema = new mongoose.Schema({
 
   about: {
     type: String,
-    default: 'Исследователь океана',
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     minlength: [2, '2 символа минимальное число символов'],
     maxlength: [30, '30 сиволов максимальное число символов'],
   },
@@ -45,21 +42,5 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
   },
 });
-
-userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new AuthenticationError('Некорректные почта или пароль'));
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new AuthenticationError('Некорректные почта или пароль'));
-          }
-          return user;
-        });
-    });
-};
 
 module.exports = mongoose.model('user', userSchema);
