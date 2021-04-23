d81221f979ad68e1c25b61292dd35bd31a677f57
Denis Sokolov <ostin1982@yandex.ru>
2021-04-23 17:12:17 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,8 +6,8 @@ const AuthenticationError = require('../errors/AuthenticationError');
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthenticationError('Необходима авторизация!!!'));
+  if (!authorization) {
+    throw new AuthenticationError('Необходима авторизация!!!');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,12 +15,12 @@ const auth = (req, res, next) => {
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
-  } catch (e) {
-    return next(new AuthenticationError('Необходима авторизация'));
+  } catch (err) {
+    throw new AuthenticationError('Необходима авторизация');
   }
 
   req.user = payload;
-  return next();
+  next();
 };
 
 module.exports = auth;
