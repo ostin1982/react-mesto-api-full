936cb6f373cac708d0d57ed31ecc573780469eb6
Denis Sokolov <ostin1982@yandex.ru>
2021-04-24 17:22:10 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -20,7 +20,7 @@ const getUsers = (req, res, next) => {
 };
 
 const getProfile = (req, res, next) => {
-  User.findById({ id: req.params._id })
+  User.findById({ id: req.params.id })
     .then((user) => {
       if (!user) {
         throw new NotFoundError('A card with such data does not exist!');
