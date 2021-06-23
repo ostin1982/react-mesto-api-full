16a68595935ac13b0836f90ab3f912f25b00e9ef
Denis Sokolov <ostin1982@yandex.ru>
2021-06-23 18:57:31 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,26 +5,26 @@ const {
   getUsers, createProfile, getProfile, updateProfile, updateAvatar,
 } = require('../controllers/users');
 
-router.get('/', getUsers);
+router.get('/users', getUsers);
 
-router.get('/me', getProfile);
+router.get('/users/me', getProfile);
 
-router.get('/:id', celebrate({
+router.get('/users/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().required().length(24).hex(),
   }),
 }), createProfile);
 
-router.patch('/me', celebrate({
+router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30).required(),
   }),
 }), updateProfile);
 
-router.patch('/me/avatar', celebrate({
+router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required(),
+    avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
   }),
 }), updateAvatar);
 
