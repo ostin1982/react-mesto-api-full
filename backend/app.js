378aed295294f2ca3f47008d867a0696069855eb
Denis Sokolov <ostin1982@yandex.ru>
2021-06-25 21:29:26 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -5,23 +5,16 @@ const mongoose = require('mongoose');
 const expressWinston = require('express-winston');
 const winston = require('winston');
 const { celebrate, errors, Joi } = require('celebrate');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const helmet = require('helmet');
-const router = require('./routes/router');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
-const allowedCors = [
-  'https://ostin.student.nomoredomains.club',
-  'http://ostin.student.nomoredomains.club',
-  'http://localhost:3001',
-  'http://localhost:3000',
-];
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -30,25 +23,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-const corsOptions = {
-  origin: allowedCors,
-  optionsSuccessStatus: 200,
-};
+app.use(cors());
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-});
-
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(expressWinston.logger({
   transports: [
@@ -79,7 +57,14 @@ app.post('/signin', celebrate({
 }),
 login);
 
-app.use('/', router);
+app.use(auth);
+
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+
+app.use('*', () => {
+  throw new NotFoundError('Карточки с такими данными не существует');
+});
 
 app.use(expressWinston.logger({
   transports: [
@@ -89,10 +74,6 @@ app.use(expressWinston.logger({
 }));
 
 app.use(errors());
-
-app.use('*', () => {
-  throw new NotFoundError('Карточки с такими данными не существует');
-});
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
