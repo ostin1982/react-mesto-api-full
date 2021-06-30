9c197305535de783c1add8a16b55987238b1f2be
Denis Sokolov <ostin1982@yandex.ru>
2021-06-30 19:07:49 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -4,7 +4,7 @@ const ProfileError = require('../errors/ProfileError');
 const NotFoundError = require('../errors/NotFoundError');
 
 const getCards = (req, res, next) => Card.find({})
-  .then((cards) => res.status(200).send(cards))
+  .then((card) => res.status(200).send(card))
   .catch(next);
 
 const createCard = (req, res, next) => {
