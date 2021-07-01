af2d14e6026e4c30add95f16fd01310cffa23fa7
Denis Sokolov <ostin1982@yandex.ru>
2021-07-01 17:53:54 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const bodyParser = require('body-parser');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', bodyParser(), usersRouter);
+router.use('/cards', bodyParser(), cardsRouter);
 router.use(auth);
 router.use('*', (req, res, next) => {
   next(new NotFoundError({ message: 'Карточки с такими данными не существует' }));
