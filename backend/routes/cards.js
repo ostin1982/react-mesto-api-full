4de79aca4a18ed1db48b52edd3d67168075d1db4
Denis Sokolov <ostin1982@yandex.ru>
2021-07-01 16:22:13 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,30 +3,29 @@ const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
-const auth = require('../middlewares/auth');
 
-router.get('/cards', auth, getCards);
+router.get('/cards', getCards);
 
-router.post('/cards', auth, celebrate({
+router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(new RegExp(/^https?:\/\/[\w\-.~:/?#[\]@!$&'()*+,;=%]{4,2048}$/)),
   }),
 }), createCard);
 
-router.delete('/cards/:cardId', auth, celebrate({
+router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).required().hex(),
   }),
 }), deleteCard);
 
-router.put('/cards/:cardId/likes', auth, celebrate({
+router.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).required().hex(),
   }),
 }), likeCard);
 
-router.delete('/cards/:cardId/likes', auth, celebrate({
+router.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).required().hex(),
   }),
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,26 +4,25 @@ const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, createProfile, getProfile, updateProfile, updateAvatar,
 } = require('../controllers/users');
-const auth = require('../middlewares/auth');
 
-router.get('/users', auth, getUsers);
+router.get('/users', getUsers);
 
-router.get('/users/me', auth, getProfile);
+router.get('/users/me', getProfile);
 
-router.get('/users/:userId', auth, celebrate({
+router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
     id: Joi.string().required().length(24).hex(),
   }),
 }), createProfile);
 
-router.patch('/users/me', auth, celebrate({
+router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30).required(),
   }),
 }), updateProfile);
 
-router.patch('/users/me/avatar', auth, celebrate({
+router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
   }),
