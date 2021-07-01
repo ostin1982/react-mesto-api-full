2ede0032264814de11568eab4e0fb47de1f09f95
Denis Sokolov <ostin1982@yandex.ru>
2021-07-01 14:29:19 +0300

    Пятнадцатый спринт. Второй коммит

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,9 @@
-require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const expressWinston = require('express-winston');
 const winston = require('winston');
 const { celebrate, errors, Joi } = require('celebrate');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const helmet = require('helmet');
 const router = require('./routes/router');
 const { login, createUser } = require('./controllers/users');
@@ -15,13 +12,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const allowedCors = [
-  'https://ostin.student.nomoredomains.club',
-  'http://ostin.student.nomoredomains.club',
-  'http://localhost:3001',
-  'http://localhost:3000',
-];
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useFindAndModify: false,
@@ -29,22 +19,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-const corsOptions = {
-  origin: allowedCors,
-  optionsSuccessStatus: 200,
-};
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-});
-
-app.use(cors(corsOptions));
 app.use(bodyParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
